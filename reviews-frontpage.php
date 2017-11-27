55245028249e042aff1e77e5da9859de66d6db23
Jackson Callaway <jcksncllwy@gmail.com>
2017-11-27 15:55:23 -0800

    reviews revamp

--- reviews-frontpage.php
+++ reviews-frontpage.php
@@ -1,24 +1,55 @@
+<?php
+	$frontpage = get_page_by_title('frontpage');
+	$reviews = get_field('reviews', $frontpage);
+	$yelp_logo = wp_get_attachment_image_url(
+		$reviews['yelp_logo'], 
+		'full'
+	);
+	$yelp_stars = wp_get_attachment_image_url(
+		$reviews['yelp_stars'], 
+		'full'
+	);
+	$ta_logo = wp_get_attachment_image_url(
+		$reviews['tripadvisor_logo'], 
+		'full'
+	);
+	$ta_stars = wp_get_attachment_image_url(
+		$reviews['tripadvisor_stars'], 
+		'full'
+	);
+
+	$social_links = get_field('social_links', $frontpage);
+	$yelp_link = $social_links['yelp'];
+	$ta_link = $social_links['tripadvisor'];
+?>
 <div class="reviews">
-
-	<div class="reviews-inner">
-
-		<div class="yelp-container">
-			<span class="yelp-review" data-review-id="iJb4pWCoqHrzGiL3fGjg3w" data-hostname="www.yelp.com">
-				Read <a href="https://www.yelp.com/user_details?userid=RxhwF9c15qAO6exR3it1pQ" rel="nofollow noopener">Lindsay G.</a>'s <a href="https://www.yelp.com/biz/wild-sf-walking-tours-san-francisco-2?hrid=iJb4pWCoqHrzGiL3fGjg3w" rel="nofollow noopener">review</a> of <a href="https://www.yelp.com/biz/eZ-Ijy9tGKbFNhnr7FWGKQ" rel="nofollow noopener">Wild SF Walking Tours</a> on <a href="https://www.yelp.com" rel="nofollow noopener">Yelp</a><script async="async" src="https://www.yelp.com/embed/widgets.js" type="text/javascript"></script>
-			</span>
-		</div>
-
-		<div class="ta-container">
-			<div id="TA_selfserveprop894" class="TA_selfserveprop">
-				<ul id="cNpsGHE0" class="TA_links 9WwtS3Tc">
-					<li id="0FZlEbQ" class="sbpbPjW">
-						<a target="_blank" href="https://www.tripadvisor.com/"><img src="https://www.tripadvisor.com/img/cdsi/img2/branding/150_logo-11900-2.png" alt="TripAdvisor"/></a>
-					</li>
-				</ul>
+	<div class="reviews-inner row no-gutters justify-content-between">
+		<div class="yelp-container col">
+			<div class="logo-rating-container">
+				<img class="logo" src="<?= $yelp_logo ?>" />
+				<img class="rating" src="<?= $yelp_stars ?>" />
 			</div>
-			<script src="https://www.jscache.com/wejs?wtype=selfserveprop&amp;uniq=894&amp;locationId=3313642&amp;lang=en_US&amp;rating=true&amp;nreviews=1&amp;writereviewlink=false&amp;popIdx=true&amp;iswide=true&amp;border=true&amp;display_version=2"></script>
+			<div class="review-container">
+				<div class="review">
+					"I had such a nice day with wild SF. If you ever want to find out something about the real San Francisco go there!"
+				</div>
+				<a class="read-more-button" href="<?= $yelp_link ?>">Read More</a>
+			</div>
 		</div>
+		<div class="ta-container col">
 
+			<div class="logo-rating-container">
+				<img class="logo" src="<?= $ta_logo ?>" />
+				<img class="rating" src="<?= $ta_stars ?>" />
+			</div>
+
+			<div class="review-container">
+				<div class="review">
+					"Wes is hilarious and I had an absolute blast learning about San Francisco and listening to his unique songs on his ukelele."
+				</div>
+				<a class="read-more-button" href="<?= $ta_link ?>">Read More</a>
+			</div>
+			
+		</div>
 	</div>
-
 </div>
---
+++ reviews-widgets.php
@@ -0,0 +1,24 @@
+<div class="reviews">
+
+	<div class="reviews-inner">
+
+		<div class="yelp-container">
+			<span class="yelp-review" data-review-id="iJb4pWCoqHrzGiL3fGjg3w" data-hostname="www.yelp.com">
+				Read <a href="https://www.yelp.com/user_details?userid=RxhwF9c15qAO6exR3it1pQ" rel="nofollow noopener">Lindsay G.</a>'s <a href="https://www.yelp.com/biz/wild-sf-walking-tours-san-francisco-2?hrid=iJb4pWCoqHrzGiL3fGjg3w" rel="nofollow noopener">review</a> of <a href="https://www.yelp.com/biz/eZ-Ijy9tGKbFNhnr7FWGKQ" rel="nofollow noopener">Wild SF Walking Tours</a> on <a href="https://www.yelp.com" rel="nofollow noopener">Yelp</a><script async="async" src="https://www.yelp.com/embed/widgets.js" type="text/javascript"></script>
+			</span>
+		</div>
+
+		<div class="ta-container">
+			<div id="TA_selfserveprop894" class="TA_selfserveprop">
+				<ul id="cNpsGHE0" class="TA_links 9WwtS3Tc">
+					<li id="0FZlEbQ" class="sbpbPjW">
+						<a target="_blank" href="https://www.tripadvisor.com/"><img src="https://www.tripadvisor.com/img/cdsi/img2/branding/150_logo-11900-2.png" alt="TripAdvisor"/></a>
+					</li>
+				</ul>
+			</div>
+			<script src="https://www.jscache.com/wejs?wtype=selfserveprop&amp;uniq=894&amp;locationId=3313642&amp;lang=en_US&amp;rating=true&amp;nreviews=1&amp;writereviewlink=false&amp;popIdx=true&amp;iswide=true&amp;border=true&amp;display_version=2"></script>
+		</div>
+
+	</div>
+
+</div>
