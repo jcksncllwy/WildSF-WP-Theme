f11682d62ace9e5671386507f6c7ce7bfc672d3f
Jackson Callaway <jacksoncallaway@Slabtop.local>
2017-12-18 16:40:01 -0800

    make CTA editable

--- section-cta.php
+++ section-cta.php
@@ -1,8 +1,15 @@
+<?php
+	$frontpage = get_page_by_title('frontpage');
+	$cta_group = get_field('call_to_action', $frontpage->ID);
+	$big_cta = $cta_group['big_cta'];
+	$little_cta = $cta_group['little_cta'];
+	$button_text = $cta_group['button_text'];
+?>
 <div class="section cta">
 	<hr />
-	<h1>See the wild side of San Francisco</h1>
-	<h2>Get out there - get wild</h2>
+	<h1><?= $big_cta ?></h1>
+	<h2><?= $little_cta ?></h2>
 
-	<a class="book-button" href="https://www.peek.com/s/15a8284c-0990-4986-a5b4-1754b0c0b014/8MW">Book now</a>
+	<a class="book-button" href="https://www.peek.com/s/15a8284c-0990-4986-a5b4-1754b0c0b014/8MW"><?= $button_text ?></a>
 	<hr />
 </div>
