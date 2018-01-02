cfea47389074f37e62fa43e4bc22b779c49956da
Jackson Callaway <jcksncllwy@gmail.com>
2018-01-02 15:09:52 -0800

    support for sightseeing pass page

--- page.php
+++ page.php
@@ -15,7 +15,10 @@
 		<?php
 			if ( have_posts() ){
 				while ( have_posts() ) : the_post();
-					the_content();
+		?>
+					<h1><?= the_title(); ?></h1>
+					<?= the_content(); ?>
+		<?php
 				endwhile;
 			}
 		?>
