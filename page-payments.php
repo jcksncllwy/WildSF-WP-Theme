3ebcf05ecabff7bd4abf37f3812b3107d9756d0b
Jackson Callaway <jcksncllwy@gmail.com>
2018-04-07 02:17:02 -0700

    designing the fields and buttons

--- page-payments.php
+++ page-payments.php
@@ -5,13 +5,13 @@
   <script src="https://js.braintreegateway.com/web/dropin/1.9.4/js/dropin.min.js"></script>
 	<script src="https://cdn.jsdelivr.net/npm/lodash@4.17.5/lodash.min.js"></script>
 </head>
-<body>
+<body class="payments-page">
 
 	<?php
 	get_template_part('navbar');
 	?>
 
-  <div class="payments">
+  <div class="payments page-container">
 
     <div class="payments-inner">
 	    <?php
@@ -26,27 +26,41 @@
 
 			<div class="payments-form">
 
-				<div class="success-message">
+				<div class="payment-success">
 					<h1>Payment Successful</h1>
+					<h2>Can't wait for your tour!</h2>
 				</div>
 
 				<div class="form-fields">
 					<div class="customer-info">
-						<div class="input-group name">
-							<span class="input-group-addon">First Name</span>
-							<input type="text" class="form-control name" id="firstName" />
-							<span class="input-group-addon">Last Name</span>
-							<input type="text" class="form-control name" id="lastName" />
+						<div class="name-fields">
+							<div class="field">
+								<div class="field-label">First Name</div>
+								<input type="text" class="form-control field-input name" id="firstNameInput" />
+							</div>
+							<div class="field">
+								<div class="field-label">Last Name</div>
+								<input type="text" class="form-control field-input name" id="lastNameInput" />
+							</div>
 						</div>
 
-						<div class="input-group">
-		        	<span class="input-group-addon">$</span>
-		        	<input type="number" value="200.00" min="0" step="0.01" data-number-to-fixed="2" data-number-stepfactor="100" class="form-control currency" id="paymentAmount" />
+						<div class="field">
+		        	<span class="field-label">$</span>
+		        	<input
+								id="paymentAmount"
+								class="form-control field-input currency"
+								type="number"
+								value="200.00"
+								min="0"
+								step="0.01"
+								data-number-to-fixed="2"
+								data-number-stepfactor="100"
+							/>
 						</div>
 					</div>
 
 			    <div id="dropin-container"></div>
-			    <button id="submit-button">Verify Payment Method</button>
+			    <button id="submit-button" class="button">Verify Payment Method</button>
 				</div>
 		    <script>
 		    $(function(){
@@ -85,13 +99,11 @@
 								data: {
 									nonce: event.data.paymentMethod.nonce,
 									amount: $('#paymentAmount').val(),
-									name: _.reduce($('.input-group.name input'),function(name,i){
-										return name+$(i).val();
-									},'')
+									name: $('#firstNameInput').val() +" "+ $('#lastNameInput').val()
 								},
 								success: function(data){
 									$('.payments-form').addClass('success');
-									console.log(data)
+									console.log(data);
 								},
 								fail: function(jqXHR, status, err){
 									alert('There was an error processing your payment!');
