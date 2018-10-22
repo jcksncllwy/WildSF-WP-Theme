774c36e9359fad9da2c67e064e7fab880c0358f1
Kelley Shanahan <kelley@trustsherpa.com>
2018-10-22 16:57:52 -0700

    updated styles on post page

--- home.php
+++ home.php
@@ -1,13 +1,26 @@
 <!DOCTYPE html>
 <html>
 <head>
+
+	<?= get_header('common') ?>
+
+	<?php
+
+		$dotted_line_image_id = 172;
+		$dotted_line_image_url = wp_get_attachment_image_src($dotted_line_image_id, 'full')[0];
+
+	?>
+
 	<style>
 		.page-container {
-			padding-top: 48px;
+			padding-top: 48px !important;
 		}
 		.page-inner {
 			background-color: white;
 			padding: 0 30px;
+		}
+		.post-summary {
+			padding: 3rem 0;
 		}
 		.dotted-line{
 			background-image: url("<?= $dotted_line_image_url ?>");
@@ -15,8 +28,10 @@
 			height: 10px;
 			background-size: contain;
 		}
+		a, a:hover {
+			color: #bf5246;
+		}
 	</style>
-	<?= get_header('common') ?>
 </head>
 <body>
 	<?php
@@ -30,8 +45,9 @@
 				while ( have_posts() ) : the_post();
 		?>
 			<div class="post-summary">
-				<h1><?= the_title(); ?></h1>
+				<h1><a href="<?php echo get_permalink(); ?>"><?= the_title(); ?></a></h1>
 				<?= the_excerpt() ?>
+				<a href="<?php echo get_permalink(); ?>"> Read More...</a>
 				<hr class="dotted-line">
 			</div>
 		<?php
