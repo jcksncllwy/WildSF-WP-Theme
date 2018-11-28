61a911ae1a5b7f021357645470b931fbfca546bd
Kelley Shanahan <kelley@trustsherpa.com>
2018-11-28 12:35:25 -0800

    added function to prevent submitting contact 7 multiple times

--- functions.php
+++ functions.php
@@ -165,3 +165,30 @@ add_action( 'rest_api_init', function () {
 } );
 
 ?>
+
+
+// Prevent Multi Submit on WPCF7 forms
+add_action( 'wp_footer', 'mycustom_wp_footer' );
+
+function mycustom_wp_footer() {
+?>
+
+<script type="text/javascript">
+var disableSubmit = false;
+jQuery('input.wpcf7-submit[type="submit"]').click(function() {
+    jQuery(':input[type="submit"]').attr('value',"Sending...")
+    if (disableSubmit == true) {
+        return false;
+    }
+    disableSubmit = true;
+    return true;
+})
+  
+var wpcf7Elm = document.querySelector( '.wpcf7' );
+wpcf7Elm.addEventListener( 'wpcf7submit', function( event ) {
+    jQuery(':input[type="submit"]').attr('value',"send")
+    disableSubmit = false;
+}, false );
+</script>
+<?php
+}
