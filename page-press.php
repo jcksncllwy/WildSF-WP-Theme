b036548f28402811ad7e2013e072ce675fa5424a
Kelley Shanahan <kelley@trustsherpa.com>
2019-02-28 16:32:26 -0800

    Switch Press Items to Custom Fields so that WildSF can edit content

--- page-press.php
+++ page-press.php
@@ -15,64 +15,33 @@
       <div class="header">
         Press
       </div>
+	  <?php if( have_rows('repeater_field_name') ): ?>
+		<div class="press-items">
+		  	<?php while( have_rows('repeater_field_name') ): the_row();
 
-      <div class="press-items">
-        <div class="press-item">
-          <a class="article-link" href="http://www.theaustralian.com.au/life/travel/hippie-history-with-a-hipster-twist-in-haightashbury-san-francisco/news-story/cd29ed87f164d3b056b493e09c48b5e8" target="_blank">
-            Hippie History with a Hipster Twist in Haight-Ashbury, San Francisco
-          </a>
-          <div class="author">The Australian</div>
-        </div>
-        <div class="press-item">
-          <a class="article-link" href="http://www.viamagazine.com/destinations/san-franciscos-best-walking-tours" target="_blank">
-            San Francisco's Best Walking Tours
-          </a>
-          <div class="author">Via Magazine</div>
-        </div>
-        <div class="press-item">
-          <a class="article-link" href="https://www.yahoo.com/travel/americas-quirkiest-cities-113456379417.html" target="_blank">
-            They're Weird and Proud: America’s Quirkiest Cities
-          </a>
-          <div class="author">Yahoo Travel</div>
-        </div>
-        <div class="press-item">
-          <a class="article-link" href="http://www.travelandleisure.com/articles/best-walking-tours-worldwide" target="_blank">
-            43 Walking Tours We Love Around the World
-          </a>
-          <div class="author">Travel + Leisure</div>
-        </div>
-        <div class="press-item">
-          <a class="article-link" href="http://www.independent.co.uk/travel/americas/san-francisco-to-los-angeles-sarfraz-manzoor-road-tests-a-friendship-on-a-drive-down-californias-a6855676.html" target="_blank">
-            San Francisco to Los Angeles
-          </a>
-          <div class="author">The Independent</div>
-        </div>
-        <div class="press-item">
-          <a class="article-link" href="http://hoodline.com/2016/01/meet-wild-sf-the-upper-haight-s-musical-tour-guides" target="_blank">
-            Meet Wild SF, The Upper Haight’s Musical Tour Guides
-          </a>
-          <div class="author">Hoodline</div>
-        </div>
-        <div class="press-item">
-          <a class="article-link" href="http://theculturetrip.com/north-america/usa/california/articles/sf-history-tours-that-wont-bore-you/ " target="_blank">
-            SF History Tours That Won't Bore You
-          </a>
-          <div class="author">The Culture Trip</div>
-        </div>
-        <div class="press-item">
-          <a class="article-link" href="http://www.traveller.com.au/san-francisco-quick-guide-gjffnf" target="_blank">
-            San Francisco City Guide: Three-minute guide to the US city of pop culture
-          </a>
-          <div class="author">Traveller</div>
-        </div>
-        <div class="press-item">
-          <a class="article-link" href="http://www.editionsnomades.com/livres/details/71/san-francisco-lessentiel/" target="_blank">
-            Travel Guide Book
-          </a>
-          <div class="author">San Francisco L'Essentiel</div>
-        </div>
-      </div>
+				// vars
+				$source = get_sub_field('source');
+				$link = get_sub_field('link');
+				if( $link ):
+					$link_url = $link['url'];
+					$link_title = $link['title'];
+			?>
 
+				<div class="press-item">
+					<?php if( $link ): ?>
+						<a class="article-link" href="<?php echo $link_url; ?>" target="_blank">
+							<?php echo $link_title; ?>
+						</a>
+					<?php endif; ?>
+					<div class="author"><?php echo $source; ?></div>
+				</div>
+
+			<?php endwhile; ?>
+
+		</div>
+
+	  <?php endif; ?>
+	  
       <div class="wp-admin-content">
     		<?php
     			if ( have_posts() ){
