365dada259c9bbf7a8a59ebb0029e1a01eac8df8
Kelley Shanahan <kelley@trustsherpa.com>
2019-10-02 17:05:00 -0500

    Add Chatbox

--- footer.php
+++ footer.php
@@ -33,4 +33,6 @@
 	</div>
 
 </div>
+<script type="text/javascript">!function(e,t,n){function a(){var e=t.getElementsByTagName("script")[0],n=t.createElement("script");n.type="text/javascript",n.async=!0,n.src="https://beacon-v2.helpscout.net",e.parentNode.insertBefore(n,e)}if(e.Beacon=n=function(t,n,a){e.Beacon.readyQueue.push({method:t,options:n,data:a})},n.readyQueue=[],"complete"===t.readyState)return a();e.attachEvent?e.attachEvent("onload",a):e.addEventListener("load",a,!1)}(window,document,window.Beacon||function(){});</script>
+<script type="text/javascript">window.Beacon('init', '6a3d3cac-402c-49db-8e2b-d2d450c7fd3a')</script>
 <?php wp_footer(); ?>
