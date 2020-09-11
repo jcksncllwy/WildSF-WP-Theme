c5c26978bd832ccec2b48cfb7939c19b7a7dedf3
Kelley Shanahan <shanahangirl@gmail.com>
2020-09-11 17:54:44 -0500

    create a schedule page template

---
+++ schedule.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<?php
+/*
+Template Name: Scheduule
+Template Post Type: page
+*/
+?>
+<html>
+<head>
+
+	<?= get_header('common') ?>
+
+</head>
+<body>
+	<?php
+		get_template_part('navbar');
+
+	?>
+	<div class="page-container">
+		<div class="page-inner">
+		<?php
+			if ( have_posts() ){
+				while ( have_posts() ) : the_post();
+		?>
+					<h1><?= the_title(); ?></h1>
+					<?= the_content(); ?>
+		<?php
+				endwhile;
+			}
+		?>
+		</div>
+	</div>
+
+<?php
+	get_template_part('footer');
+?>
+
+</body>
+</html>
