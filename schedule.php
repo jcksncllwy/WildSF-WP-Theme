11d3ec9852737b7306f769dae2bd8ba37d03d03c
Kelley Shanahan <shanahangirl@gmail.com>
2020-09-11 18:30:28 -0500

    setup main sections

--- schedule.php
+++ schedule.php
@@ -9,8 +9,20 @@ Template Post Type: page
 <head>
 
 	<?= get_header('common') ?>
-
+	<?php
+		$dotted_line_image_id = 172;
+	  	$dotted_line_image_url = wp_get_attachment_image_src($dotted_line_image_id, 'full')[0
+	?>
+	<style type="text/css" class="wp-dynamic-css">
+	  .dotted-line{
+	    background-image: url("<?= $dotted_line_image_url ?>");
+	    border: 0;
+	    height: 10px;
+	    background-size: contain;
+	  }
+	</style>
 </head>
+
 <body>
 	<?php
 		get_template_part('navbar');
@@ -18,12 +30,21 @@ Template Post Type: page
 	?>
 	<div class="page-container schedule-page">
 		<div class="container">
-			<?php
-				if ( have_posts() ){
-					while ( have_posts() ) : the_post();
-			?>
-						<h1><?= the_title(); ?></h1>
-						<?= the_content(); ?>
+			<div class="top-content row">
+				<?php
+					if ( have_posts() ){
+						while ( have_posts() ) : the_post();
+				?>
+				<h1><?= the_title(); ?></h1>
+				<?= the_content(); ?>
+			</div>
+			<div class="schedule">
+				<hr class="dotted-line" />
+				<div class="schedule-head">
+					Schedule of Events
+				</div>
+				<hr class="dotted-line" />
+			</div>
 			<?php
 					endwhile;
 				}
